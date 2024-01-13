ad4602b6dc91ebec83a446d7215c7362a7814d51
sameer khan <sameerkhanaa5@gmail.com>
2024-01-13 12:14:07 +0530

    Filter Product Final

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -8,25 +8,33 @@ function Products() {
   const { state } = useContext(ShopingContext);
   const [sortedArr, setSortedArr] = useState(state.data);
   const [value, setValue] = useState("");
+
+  const price = state.data
+    .map((item) => item.fields.price)
+    .sort((a, b) => b - a);
+
+  const [rangeVlaue, setRangeValue] = useState(price[0]);
+
   const allCompanys = [
     ...new Set(state.data.map((item) => item.fields.company)),
   ];
-
-  const [highestPrice] = state.data
-    .map((item) => item.fields.price)
-    .sort((a, b) => b - a);
-  console.log(highestPrice);
 
   useEffect(() => {
     searchSort();
   }, [value]);
 
+  useEffect(() => {
+    sortByRange(rangeVlaue);
+  }, [rangeVlaue]);
+
+  // Sort By Search
   function searchSort() {
     setSortedArr(
       state.data.filter((item) => item.fields.name.startsWith(value))
     );
   }
 
+  // Sort By Company
   function sortByCompany(e) {
     if (e.target.textContent === "All") {
       setSortedArr(state.data);
@@ -37,6 +45,16 @@ function Products() {
     );
     setSortedArr(filteredArr);
   }
+
+  // Sort By Range
+  function sortByRange(range) {
+    const sortedRange = state.data.filter(
+      (item) => item.fields.price <= rangeVlaue
+    );
+    setSortedArr(sortedRange);
+  }
+
+  // Rander Arr
 
   function renderArr(arr) {
     return arr.map((item) => <FeatureProduct product={item} key={item.id} />);
@@ -67,7 +85,17 @@ function Products() {
             </ul>
           </div>
           <p className="sub-heading">Price</p>
-          <input type="range" />
+          <input
+            type="range"
+            className="range-input"
+            min={price[price.length - 1]}
+            max={price[0]}
+            value={rangeVlaue}
+            onChange={(e) => {
+              setRangeValue(e.target.value);
+            }}
+          />
+          <p className="price">${rangeVlaue}</p>
         </aside>
         <article className="product-div">
           {sortedArr.length <= 0 && <h1>Product Not Found</h1>}
